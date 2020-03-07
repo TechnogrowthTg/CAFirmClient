8b2d831396c424cf582ae221c1a5db3e8a50f5eb
RaviMrk <raviparmar12316@gmail.com>
2020-03-07 14:57:22 +0530

    material added

---
+++ src/material.module.ts
@@ -0,0 +1,89 @@
+
+import { NgModule } from "@angular/core";
+import {
+    MatDialogModule, MatSortModule, MatCardModule,
+    MatTabsModule, MatTableModule, MatIconModule,
+    MatPaginatorModule, MatFormFieldModule, MatSelectModule,
+    MatInputModule, MatButtonModule, MatDividerModule,
+    MatGridListModule, MatListModule, MatCheckboxModule,
+    MatDatepickerModule, MatNativeDateModule, MatMenuModule,
+    MatRadioModule, MatExpansionModule, MatTooltipModule, MatSlideToggleModule, MatStepperModule,
+    MatAutocompleteModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatBottomSheetModule,
+} from "@angular/material";
+
+
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+@NgModule({
+    imports: [
+      MatSidenavModule,
+        MatDialogModule,
+        MatSortModule,
+        MatCardModule,
+        MatTabsModule,
+        MatTableModule,
+        MatIconModule,
+        MatPaginatorModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatInputModule,
+        MatButtonModule,
+        MatDividerModule,
+        MatGridListModule,
+        MatListModule,
+        MatCheckboxModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatMenuModule,
+        MatRadioModule,
+        MatExpansionModule,
+        MatTooltipModule,
+        MatSlideToggleModule,
+        
+        MatStepperModule,
+        MatAutocompleteModule,
+        ReactiveFormsModule,
+        
+        MatToolbarModule,
+        MatBottomSheetModule,
+        
+        FormsModule
+    ],
+    exports: [
+      MatSidenavModule,
+        MatDialogModule,
+        MatSortModule,
+        MatCardModule,
+        MatTabsModule,
+        MatTableModule,
+        MatIconModule,
+        MatPaginatorModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatInputModule,
+        MatButtonModule,
+        MatDividerModule,
+        MatGridListModule,
+        MatListModule,
+        MatCheckboxModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatRadioModule,
+        MatExpansionModule,
+        MatTooltipModule,
+        MatSlideToggleModule,
+        
+        MatStepperModule,
+        MatAutocompleteModule,
+        ReactiveFormsModule,
+        
+        MatToolbarModule,
+        MatBottomSheetModule,
+        
+        FormsModule
+    ],
+})
+export class MaterialModule { }
