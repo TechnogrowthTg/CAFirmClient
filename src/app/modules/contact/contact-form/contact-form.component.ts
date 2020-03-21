e76853bf589feb5ccac9b90368bac2a25cec0de7
RaviMrk <raviparmar12316@gmail.com>
2020-03-21 14:31:32 +0530

    contact form

--- src/app/modules/contact/contact-form/contact-form.component.ts
+++ src/app/modules/contact/contact-form/contact-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpService } from 'src/app/services/http.service';
+import { ToastrManager } from 'ng6-toastr-notifications';
+import { FormGroup, FormControl } from '@angular/forms';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-contact-form',
@@ -7,9 +12,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactFormComponent implements OnInit {
 
-  constructor() { }
+
+  response: any;
+
+
+  constructor(private httpService: HttpService, private router: Router, private toaster: ToastrManager) { 
+
+  }
+  
+
+
+
+  contactForm = new FormGroup({
+    ContactPersonName: new FormControl(),
+    email: new FormControl(),
+    designationName: new FormControl(),
+    MobileNumber1: new FormControl(),
+    MobileNumber2: new FormControl(),
+    telephone: new FormControl(),
+    address: new FormControl(),
+    Reference: new FormControl(),
+    currentStatus: new FormControl()
+  });
+
 
   ngOnInit() {
+  }
+
+  submitContactForm(){
+
+    if(this.contactForm.valid){
+
+      this.httpService.postSecured(environment.postClientData,this.contactForm.value).subscribe(data=>{
+        this.response=data;
+
+        this.router.navigate(['home/client']);
+
+      })
+
+    }
+  }
+
+  cancelForm(){
+    this.contactForm.reset()
+    this.router.navigate(['home/client']);
   }
 
 }
